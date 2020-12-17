ca4450060a6ac08ea3ef899678aa60c917987de8
Stellupo <e.braido@gmail.com>
2020-12-17 16:27:26 +0100

    JavaScript Project : stage 2 - buttons check, edit & delete, design improvements

--- InteractiveTodoList/script.js
+++ InteractiveTodoList/script.js
@@ -20,7 +20,7 @@
 let array =
 */
 
-// checking if the tasks list is empty
+// updating the visual of the page according to the presence of tasks in tasks_list
 function listUpdate(list) {
     let section = document.getElementById("section");
     let footer = document.querySelector('footer');
@@ -39,10 +39,25 @@ function listUpdate(list) {
     }
 
     // if list is empty, it disappears, the titles & the footer come back
-    list.classList.toggle("close");
-    section.querySelector('h2').classList.toggle("close");
-    section.querySelector('h3').classList.toggle("close");
-    footer.classList.toggle('close');
+    list.classList.add("close");
+    section.querySelector('h2').classList.remove("close");
+    section.querySelector('h3').classList.remove("close");
+    footer.classList.remove('close');
+}
+
+// creating the element for the new task
+function createTask(title) {
+    let task = document.createElement('div');
+    task.id = 'task';
+    let task_name = document.createElement('p');
+    task_name.innerHTML = title; // adding the title of the task to our element p
+
+    task.append(task_name);
+
+    // adding the buttons check, edit and delete
+    task.insertAdjacentHTML('beforeend', '<div id="img_wrapper"><img src="Images/icon_check_green.png" alt="icon 1"><img src="Images/icon_edit_white.png" alt="icon 2"> <img src="Images/icon_delete_red.png" alt="icon 3"></div>');
+
+    task_list.append(task);
 }
 
 
@@ -57,12 +72,13 @@ addButton.addEventListener("click", function() {
     let title = input_bar.value;
 
     // if the title is empty
-    if (title == "") alert ("Oh, Silly! You need a name for your task :o");
+    if (title == "") {
+        alert ("Oh, Silly! You need a name for your task :o");
+        return
+    }
 
     // if the title is complete
-    let task = document.createElement('p');
-    task.innerHTML = title; // adding the title of the task to our element p
-    task_list.append(task);
+    createTask(title);
     listUpdate(task_list);
 });
 
@@ -71,7 +87,7 @@ let clearButton = document.getElementById('clear_tasks');
 
 clearButton.addEventListener("click", function() {
     // getting the tasks in the list
-    let tasks = tasks_list.querySelectorAll('p');
+    let tasks = tasks_list.querySelectorAll('div');
 
     // removing the tasks one by one
     for (let task of tasks) {
@@ -83,9 +99,10 @@ clearButton.addEventListener("click", function() {
 
 
 /* todo
-creer une tache et shooter la partie haute h2 et h3 dans section
+creer une tache et shooter la partie haute h2 et h3 dans section - done
 faire le style du div tasks list
 penser au local storage
+completer la tâche
 pouvoir la suppr
 modifier la tache
 alerter quand on essaie d'ajouter une tâche vite
